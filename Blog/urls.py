36ae3a692365f22e1f01d9b8446f4082505435d5
Ari Beri <berinsteinariel@gmail.com>
2021-06-09 19:28:09 -0300

    url categorias.html

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import blog
+from .views import blog, categoria
 
 urlpatterns = [
     path('', blog, name="Blog"),
+    path('categoria/<categoria_id>/', categoria, name="Categorias")
 
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from Blog.models import Post
+from Blog.models import Post, Categoria
 
 
 def blog(request):
@@ -7,3 +7,11 @@ def blog(request):
     posts = Post.objects.all()
 
     return render(request, "Blog/blog.html", {"posts": posts})
+
+def categoria(request, categoria_id):
+
+    categorias = Categoria.objects.get(id=categoria_id)
+    
+    posts = Post.objects.filter(categorias=categorias)
+
+    return render(request, "Blog/categorias.html", {"categorias": categorias, "posts": posts})
