27e421641791dc97d0aa4e0cdbeb79cfe1918aed
Ari Beri <berinsteinariel@gmail.com>
2021-06-10 20:03:30 -0300

    bg_main picture

--- Contacto/views.py
+++ Contacto/views.py
@@ -6,4 +6,11 @@ def contacto(request):
     
     formulario = FormularioContacto()
 
+    if request.method=="POST":
+        formulario=FormularioContacto(data=request.POST)
+        if formulario.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            mensaje=request.POST.get("mensaje")
+
     return render(request, "Contacto/contacto.html", {"formulario": formulario})
