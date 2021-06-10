850e2806c45fb9bf507cae98567d702351adf9d3
Ari Beri <berinsteinariel@gmail.com>
2021-06-10 20:28:23 -0300

    email

--- Contacto/views.py
+++ Contacto/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 
 def contacto(request):
@@ -12,5 +13,16 @@ def contacto(request):
             nombre=request.POST.get("nombre")
             email=request.POST.get("email")
             mensaje=request.POST.get("mensaje")
+
+            email = EmailMessage("Mensaje desde Hype Market Web", 
+            "El usuario {} con la dirección {} escribe lo siguiente:\n\n {}".format(nombre, email, mensaje),
+            "",["berinsteina@gmail.com"], reply_to=[email])
+
+            try:
+                email.send()
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")
+
 
     return render(request, "Contacto/contacto.html", {"formulario": formulario})
--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -132,3 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+#config de email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'berinsteina@gmail.com'
+EMAIL_HOST_PASSWORD = 'Independiente@140799'
