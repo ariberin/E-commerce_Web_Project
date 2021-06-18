47e3bdb72cc3a74dc5f17aca4e3065e6c6737c0f
Ari Beri <berinsteinariel@gmail.com>
2021-06-18 12:47:24 -0300

    carrito

--- Carrito/views.py
+++ Carrito/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .carrito import Carrito
+from Tienda.models import Producto
 
-# Create your views here.
+def agregar_producto(request, producto_id):
+
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    
+    carrito.agregar(producto=producto)
+
+    return redirect("tienda")
+
+def eliminar_producto(request, producto_id):
+
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    
+    carrito.eliminar(producto=producto)
+
+    return redirect("tienda")
+
+def restar_producto(request, producto_id):
+
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+
+    carrito.restar(producto=producto)
+
+    return redirect("tienda")
+
+def limpiar_carrito(request):
+
+    carrito = Carrito(request)
+    carrito.limpiar()
+
+    return redirect("tienda")
