0055919bb4231aeb00c5397ac937b62d5935e39b
Ari Beri <berinsteinariel@gmail.com>
2021-06-17 22:21:50 -0300

    carrito

---
+++ Carrito/carrito.py
@@ -0,0 +1,59 @@
+
+class Carrito:
+
+    def __init__(self, request):
+
+        self.request = request
+        self.session = request.session
+        
+        carrito = self.session.get("carrito")
+
+        if not carrito:
+            carrito = self.session['carrito'] = {}
+        else:
+
+            self.carrito = carrito
+    
+    def agregar(self, producto):
+
+        if str(producto.id) not in self.carrito.keys():
+            self.carrito[producto.id] = {
+                "producto_id":producto.id,
+                "nombre":producto.nombre,
+                "precio":str(producto.precio),
+                "cantidad":1,
+                "imagen":producto.imagen.url
+            }
+        else:
+            for k, v in self.carrito.items():
+                if k == str(producto.id):
+                    v["cantidad"] += 1
+                    break
+        
+        self.guardar()
+
+    def guardar(self):
+
+        self.session["carrito"] = self.carrito
+        self.session.modified = True
+    
+    def eliminar(self, producto):
+
+        if str(producto.id) in self.carrito:
+            del self.carrito[producto.id]
+            self.guardar()
+    
+    def restar(self, producto):
+
+        for k, v in self.carrito.items():
+            if k == str(producto.id):
+                v["cantidad"] -= 1
+                if v["cantidad"] < 1:
+                    self.eliminar()
+                break
+        self.guardar()
+    
+    def limpiar(self):
+
+        self.session["carrito"] = {}
+        self.session.modified = True
